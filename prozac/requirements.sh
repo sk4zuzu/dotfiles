69f3a5e73f1b25179a3d3708eec0954e88076187
Michal Opala <sk4zuzu@gmail.com>
2019-07-03 13:19:34 +0200

    prozac: updating requirements.sh

--- prozac/requirements.sh
+++ prozac/requirements.sh
@@ -10,6 +10,7 @@ set -e
 : ${TERRAFORM_PROVIDER_NULL_VERSION:=2.1.2}
 : ${TERRAFORM_PROVIDER_LOCAL_VERSION:=1.3.0}
 : ${TERRAFORM_PROVIDER_TEMPLATE_VERSION:=2.1.2}
+: ${TERRAFORM_PROVIDER_TERRAFORM_VERSION:=1.0.2}
 : ${TERRAFORM_PROVIDER_GOOGLE_VERSION:=2.9.1}
 
 set -o errexit -o nounset -o pipefail
@@ -19,6 +20,10 @@ install -d -m u=rwx,go= ${GOPATH}/{,bin/}
 
 terragrunt_release_install() {
     local VERSION="$1"; shift
+
+    if [ -x ${GOPATH}/bin/terragrunt ]; then
+        return
+    fi
 
     curl -fSL https://github.com/gruntwork-io/terragrunt/releases/download/v${VERSION}/terragrunt_linux_amd64 \
          -o ${GOPATH}/bin/terragrunt \
@@ -31,6 +36,10 @@ hashicorp_release_install() {
     local NAME="$1"; shift
     local VERSION="$1"; shift
 
+    if [ -x ${GOPATH}/bin/${NAME} ]; then
+        return
+    fi
+
     curl -fSL https://releases.hashicorp.com/${NAME}/${VERSION}/${NAME}_${VERSION}_linux_amd64.zip \
     | zcat >${GOPATH}/bin/${NAME} \
     && chmod +x ${GOPATH}/bin/${NAME}
@@ -41,6 +50,7 @@ hashicorp_release_install terraform $TERRAFORM_VERSION
 hashicorp_release_install terraform-provider-null $TERRAFORM_PROVIDER_NULL_VERSION
 hashicorp_release_install terraform-provider-local $TERRAFORM_PROVIDER_LOCAL_VERSION
 hashicorp_release_install terraform-provider-template $TERRAFORM_PROVIDER_TEMPLATE_VERSION
+hashicorp_release_install terraform-provider-terraform $TERRAFORM_PROVIDER_TERRAFORM_VERSION
 hashicorp_release_install terraform-provider-google $TERRAFORM_PROVIDER_GOOGLE_VERSION
 
 # vim:ts=4:sw=4:et:syn=sh:
