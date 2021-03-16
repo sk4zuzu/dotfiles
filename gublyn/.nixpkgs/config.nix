821c262b2a65b41140a177cbdbcce87e6f119c7b
sk4zuzu <sk4zuzu@gmail.com>
2021-03-16 10:38:10 +0100

    gublyn: add python 3 with extra packages

--- gublyn/.nixpkgs/config.nix
+++ gublyn/.nixpkgs/config.nix
@@ -1,19 +1,26 @@
 {
   allowUnfree = true;
   packageOverrides = pkgs: {
-    local = with pkgs; buildEnv {
-      name = "local";
-      paths = [
-        neovim mc htop
-        zip unzip
-        bash_5 which
-        curl jq
-        git stow
-        cdrkit
-        docker_compose
-        gnumake
-        rustup lldb clang
-      ];
-    };
+    local = with pkgs;
+      let
+        python-pkgs = python-pkgs: with python-pkgs; [
+          pysdl2
+        ];
+        python3-with-pkgs = python3.withPackages python-pkgs;
+      in buildEnv {
+        name = "local";
+        paths = [
+          neovim mc htop
+          zip unzip
+          bash_5 which
+          curl jq
+          git stow
+          cdrkit
+          docker_compose
+          gnumake
+          rustup lldb clang
+          python3-with-pkgs
+        ];
+      };
   };
 }
