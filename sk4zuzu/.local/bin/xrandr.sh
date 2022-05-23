d0326783f68139f04485188288f343142b0f596b
sk4zuzu <sk4zuzu@gmail.com>
2022-05-24 00:33:11 +0200

    sk4zuzu: update

--- sk4zuzu/.local/bin/xrandr.sh
+++ sk4zuzu/.local/bin/xrandr.sh
@@ -3,8 +3,18 @@
 # Improve error handling.
 set -o errexit -o nounset -o pipefail
 
-if which xbacklight; then
-    xbacklight -set 100
+if [[ -f /sys/class/drm/card0/device/power_dpm_force_performance_level ]]; then
+    echo low \
+    | sudo tee /sys/class/drm/card0/device/power_dpm_force_performance_level
+fi
+
+if [[ -f /sys/class/backlight/amdgpu_bl0/brightness ]]; then
+    cat /sys/class/backlight/amdgpu_bl0/max_brightness \
+    | sudo tee /sys/class/backlight/amdgpu_bl0/brightness
+else
+    if which xbacklight; then
+        xbacklight -set 100 || true
+    fi
 fi
 
 if which hsetroot; then
@@ -12,13 +22,11 @@ if which hsetroot; then
 fi
 
 if which xrandr; then
-    if xrandr | grep "eDP1"; then
-        xrandr --output "eDP1" --auto
-        if xrandr | grep "HDMI1"; then
-            xrandr --output "HDMI1" --auto --left-of "eDP1"
-            xrandr --output "HDMI1" --primary
+    if xrandr | grep "eDP"; then
+        xrandr --output "eDP" --auto
+        if xrandr | grep "HDMI-A-0"; then
+            xrandr --output "HDMI-A-0" --auto --left-of "eDP"
+            xrandr --output "HDMI-A-0" --primary
         fi
     fi
 fi
-
-# vim:ts=4:sw=4:et:syn=sh:
